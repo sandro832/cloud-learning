b50c27c1b56cfa0aae456dcc1ed6313572414215
Sandro Graf <Sandro_Graf@swissre.com>
2020-07-01 08:40:50 +0200

    Made some changes

--- PowerShell/Create-VM-Linux.ps1
+++ PowerShell/Create-VM-Linux.ps1
@@ -1,46 +1,31 @@
 
-# Will Create a Ubuntu Linux VM
+# Requires storage account for boot diagnostics in same subscription, it will choose one if you don't specify
 
-# Assumptions:
-#  1. You have a already created a Resource Group, Network Interface, Network Security Group and Public IP.
-#  2. That you have associated the Public IP with the Network Interface.
-#  3. You have a storage account setup in the resource group.
-#  4. You have a SSH key for remote desktop.
+$GroupName = 'Sandro-Test'
+$Location  = 'westeurope'
 
-#Connect-AzAccount -SubscriptionId 'e67a50bc-70b2-4838-8598-c9fb1880a4ac'
-
-$rgName = 'sandro-test'
-$location = "westeurope"
-$pathPubKey = 'C:\srdev\Keys\2020-Q3-Pub.txt'
-
-$newVmName = 'psdemo-linux-2b'
-$username = 'demoadmin'
-
-$nicName = 'Sandro-NIC'
 $VnetName      = 'psdemo-vnt'
 $CIDR          = '172.17.0.0/16'
-$SubnetAddress = '172.17.1.0/24'
 $SubnetName    = 'psdemo-subnet-1'
-$PipName       = 'psdemo-pip-linux'
-$NsgName       = 'psdemo-nsg-linux'
-$NicName       = 'psdemo-nic-linux'
+$SubnetAddress = '172.17.1.0/24'
 
- 
+$PipName       = 'psdemo-pip-linux'
+
+$NsgName       = 'psdemo-nsg-linux'
+
+$NicName       = 'psdemo-nic-linux'
 
 $VmName        = 'psdemo-vm-linux'
 $VmSize        = 'Standard_D2_v2'
 
- 
-
 $Username      = 'demoadmin'
 $PathPubKey    = 'C:\srdev\Keys\2020-Q3-Pub.txt'
 
+$rg = New-AzResourceGroup -Name $GroupName -Location $Location
+#$rg = Get-AzResourceGroup -Name $GroupName 
 
-
-$rg = New-AzResourceGroup -Name $rgName -location $location
-
-#$rg = Get-AzResourceGroup -Name $rgName
 $rg
+
 
 $subnetConfig = New-AzVirtualNetworkSubnetConfig `
     -Name $SubnetName `
@@ -53,20 +38,19 @@ $vnet = New-AzVirtualNetwork `
     -Location $rg.Location `
     -Name $VnetName `
     -AddressPrefix $CIDR `
-   -Subnet $subnetConfig
-  
-  
- #$vnet = Get-AzVirtualNetwork -ResourceGroupName $rg.ResourceGroupName -Name $VnetName
+    -Subnet $subnetConfig
 
-    $vnet 
+#$vnet = Get-AzVirtualNetwork -ResourceGroupName $rg.ResourceGroupName -Name $VnetName
+$vnet
+
 
 $pip = New-AzPublicIpAddress `
-    -ResourceGroupName $rgName `
-    -Location $location `
+    -ResourceGroupName $rg.ResourceGroupName `
+    -Location $rg.Location `
     -Name $PipName `
     -AllocationMethod Static
-
 $pip
+
 
 $rule1 = New-AzNetworkSecurityRuleConfig `
     -Name ssh-rule `
@@ -75,23 +59,87 @@ $rule1 = New-AzNetworkSecurityRuleConfig `
     -Protocol Tcp `
     -Direction Inbound `
     -Priority 100 `
-    -SourceAddressPrefix 193.134.170.35/32 `
+    -SourceAddressPrefix '193.134.170.35/32' `
     -SourcePortRange * `
     -DestinationAddressPrefix * `
     -DestinationPortRange 22
 
 $rule1
 
-
 $nsg = New-AzNetworkSecurityGroup `
-    -ResourceGroupName $rgName`
-    -Location $location `
+    -ResourceGroupName $rg.ResourceGroupName `
+    -Location $rg.Location `
     -Name $NsgName `
     -SecurityRules $rule1
 
-#$nsg = Get-AzNetworkSecurityGroup -ResourceGroupName $rg.ResourceGroupName -Name $NsgName
-$nsg
 
+#$nsg = Get-AzNetworkSecurityGroup -ResourceGroupName $rg.ResourceGroupName -Name $NsgName
+$nsg | more
+
+
+$subnet = $vnet.Subnets | Where-Object { $_.Name -eq $SubnetName }
+
+$nic = New-AzNetworkInterface `
+    -ResourceGroupName $rg.ResourceGroupName `
+    -Location $rg.Location `
+    -Name $NicName `
+    -Subnet $subnet `
+    -PublicIpAddress $pip `
+    -NetworkSecurityGroup $nsg
+
+#$nic = Get-AzNetworkInterface -ResourceGroupName $rg.ResourceGroupName -Name $NicName
+$nic
+
+
+$LinuxVmConfig = New-AzVMConfig `
+    -VMName $VmName `
+    -VMSize $VmSize
+
+$password = ConvertTo-SecureString 'ignorepassword123412123$%^&*' -AsPlainText -Force
+$LinuxCred = New-Object System.Management.Automation.PSCredential ($Username, $password)
+
+$LinuxVmConfig = Set-AzVMOperatingSystem `
+    -VM $LinuxVmConfig `
+    -Linux `
+    -ComputerName $VmName `
+    -DisablePasswordAuthentication `
+    -Credential $LinuxCred
+
+
+$sshPublicKey = Get-Content $pathPubKey
+
+Add-AzVMSshPublicKey `
+    -VM $LinuxVmConfig `
+    -KeyData $sshPublicKey `
+    -Path "/home/$Username/.ssh/authorized_keys"
+
+$LinuxVmConfig = Set-AzVMSourceImage `
+    -VM $LinuxVmConfig `
+    -PublisherName 'Canonical' `
+    -Offer 'UbuntuServer' `
+    -Skus '19.04' `
+    -Version 'latest' 
+
+$LinuxVmConfig = Add-AzVMNetworkInterface `
+    -VM $LinuxVmConfig `
+    -Id $nic.Id 
+
+New-AzVM `
+    -ResourceGroupName $rg.ResourceGroupName `
+    -Location $rg.Location `
+    -VM $LinuxVmConfig
+
+$MyIP = Get-AzPublicIpAddress `
+    -ResourceGroupName $rg.ResourceGroupName `
+    -Name $pip.Name | Select-Object -ExpandProperty IpAddress
+
+"$Username@$MyIP"
+
+#Uncomment, select line and press F8 to run individual command:
+
+#Stop-AzVM -Name $newVmName -ResourceGroupName $rgName -Force -AsJob
+#Start-AzVM -Name $newVmName -ResourceGroupName $rgName -AsJob
+#Remove-AzVM -Name $newVmName -ResourceGroupName $rgName -Force -AsJob 
 
 
 
