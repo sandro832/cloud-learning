e39964854cda3f244c1f99d7d6d416bd21aaac77
Sandro Graf <Sandro_Graf@swissre.com>
2020-07-06 11:28:52 +0200

    small changes

--- PowerShell/Change-VM-Size.ps1
+++ PowerShell/Change-VM-Size.ps1
@@ -1,8 +1,11 @@
 $resourceGroup = "Sandro-Test"
 $vmName = "Linux"
 
+# Shows all images that are available in the region
 Get-AzVMSize -ResourceGroupName $resourceGroup -VMName $vmName 
 
 $vm = Get-AzVM -ResourceGroupName $resourceGroup -VMName $vmName
-$vm.HardwareProfile.VmSize = "Standard_D2s_v3"
+$vm.HardwareProfile.VmSize = ""
 Update-AzVM -VM $vm -ResourceGroupName $resourceGroup
+
+# For updowngrade just select the upper/downer image
