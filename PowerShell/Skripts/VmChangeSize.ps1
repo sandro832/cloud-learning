148244cb2725b70bb3546ca3fb487ff9988c110f
Sandro Graf <Sandro_Graf@swissre.com>
2020-07-23 13:30:53 +0200

    Cleanup

--- PowerShell/Skripts/VmChangeSize.ps1
+++
@@ -1,8 +0,0 @@
-$resourceGroup = "Sandro-Test"
-$vmName = "Linux"
-
-Get-AzVMSize -ResourceGroupName $resourceGroup -VMName $vmName 
-
-$vm = Get-AzVM -ResourceGroupName $resourceGroup -VMName $vmName
-$vm.HardwareProfile.VmSize = "Standard_DS3_v2"
-Update-AzVM -VM $vm -ResourceGroupName $resourceGroup
