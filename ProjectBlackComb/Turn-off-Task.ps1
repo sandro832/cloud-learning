35af9571de9f39ed31fa46471b32cc19c7c0abcb
Sandro Graf <Sandro_Graf@swissre.com>
2020-07-23 14:09:25 +0200

    Formatting

--- ProjectBlackComb/Turn-off-Task.ps1
+++ ProjectBlackComb/Turn-off-Task.ps1
@@ -2,37 +2,20 @@
 
 param($Timer)
 
-
-
 # Get the current universal time in the default string format.
 
 $currentUTCtime = (Get-Date).ToUniversalTime()
 
-
-
-Write-Host "PowerShell timer trigger function ran! TIME: $currentUTCtime"
-
-#>
-
 Select-AzSubscription e67a50bc-70b2-4838-8598-c9fb1880a4ac
-
-
 
 $vmList = Get-AzVM -Status
 
 Foreach ($machine in $vmList) {
-
-    #$ActiveVm = Write-Host $PowerOffVms = $machine.Name "---" $machine.PowerState "---" $machine.Tags
-    # To do If Powerstate VM running
     if ($machine.PowerState -eq "VM running") {
-
-        if ($machine.tags.ContainsKey("PowerOff")){
-
+        if ($machine.tags.ContainsKey("PowerOff")) {
             Write-Host  "Found Override"  $machine.Name "---" $machine.ResourceGroupName
         }
-        Else 
-        {
-            
+        Else {
             Stop-AzVM -ResourceGroupName $machine.ResourceGroupName -name $machine.name -Force -NoWait
         }
     }
