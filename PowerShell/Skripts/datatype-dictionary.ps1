df983736997c2e0dfcfa4269f132bfa2ea681d25
Sandro Graf <Sandro_Graf@swissre.com>
2020-08-18 15:02:29 +0200

    PS with Hashtable and Dictionary

---
+++ PowerShell/Skripts/datatype-dictionary.ps1
@@ -0,0 +1,43 @@
+﻿
+""
+"**************************"
+"*** Dictionary ***"
+""
+
+$dictionary = New-Object 'system.collections.generic.dictionary[string,string]'
+$dictionary.Add("key1", "value1")
+$dictionary.Add("PowerOff", "Override")
+$dictionary.Add("APM", "ILCMR")
+$dictionary.Add("Department", "P&C")
+
+$dictionary.gettype()
+
+
+$dictionary.Keys.ForEach({"$_ "}) -join ' | '
+
+# Add $dictionary["<key>"] = "<value>" overwrites value (no error), adding a duplicate with .Add("<key", "<value>") throws an error
+
+$dictionary["DB"] = "214"
+$dictionary["DB"] = "213"
+$dictionary.Add("DB", "215")   # throws an error
+$dictionary.Add["DB", "215"]
+
+"Remove called twice:"
+$dictionary.Remove("DB") #returns true
+$dictionary.Remove("DB") #returns false
+
+$dictionary.Remove["DB"]
+
+"Add new item returns no bool: " + $dictionary.Add("Checker", "215") # doesn't return true/false
+
+"Access deleted item: " + $dictionary["DB"]
+"Access deleted item: " + $dictionary.Item["DB"]
+
+"Access deleted item: " + $dictionary.Item("DB")  #will throw an error
+
+"Access existing item" + $dictionary["Department"]
+
+"ContainsKey " + $dictionary.ContainsKey("DB")
+
+$dictionary
+
---
+++ PowerShell/Skripts/datatype-hashtable.ps1
@@ -0,0 +1,41 @@
+﻿
+# https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_hash_tables?view=powershell-7
+
+""
+"**************************"
+"*** Hashtable ***"
+""
+
+$hashtable = @{
+    "key1"        = "value1"
+    "PowerOff"    = "Override"
+    "APM"         = "ILCMR"
+    "Department"  = "P&C"
+    }
+
+$hashtable.gettype()
+$hashtable
+
+$hashtable.Keys.ForEach({"$_ "}) -join ' | '
+
+# Add $hashtable["<key>"] overwrites value (no error), adding a duplicate with .Add("<key", "<value>") throws an error
+$hashtable["DB"] = "214"
+$hashtable["DB"] = "213"
+#$hashtable.Add("DB", "215")
+$hashtable.Add["DB", "215"]
+
+# Duplicate Remove is ignored, returns nothing
+$hashtable.Remove("DB")
+$hashtable.Remove("DB")
+
+$hashtable.Remove["DB"]
+
+"Access deleted item: " + $hashtable["DB"]
+"Access deleted item: " + $hashtable.Item["DB"]
+"Access deleted item: " + $hashtable.Item("DB")  
+
+"Access existing item: " + $hashtable["Department"]
+
+$hashtable
+
+
